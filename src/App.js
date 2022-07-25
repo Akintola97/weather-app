f9f8a6f8af9691378567d5b946fb43d4c5e32b4e
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2022-07-25 14:15:46 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -35,18 +35,18 @@ function App() {
   
   
   return (
-    <>
-      <div className='w-full h-full text-center'>
+    <div className='w-full h-[100vh] text-center relative'>
+      <div>
       {typeof weatherData.main === 'undefined' ? (
         <div>
           <h1 className='font-bold text-[5vmin]'>Please enter a Location</h1>
         </div>
       ):(
         <div>
-           <h1 className='location text-[15vmin]'>{weatherData.name}</h1>
+           <h1 className='location text-[10vmin]'>{weatherData.name}</h1>
            <div className='flex justify-center'>
-           <h1 className='temperature text-[10vmin]'>{weatherData.main.temp}°F </h1>
-           <h1 className='description text-[10vmin] pl-8'>{weatherData.weather[0].main}</h1>
+           <h1 className='temperature text-[7vmin]'>{weatherData.main.temp}°F </h1>
+           <h1 className='description text-[7vmin] pl-8'>{weatherData.weather[0].main}</h1>
        </div>
         </div>
       )
@@ -62,24 +62,24 @@ function App() {
       }
        
       </div>
-        <div className='fixed top-[50%] left-[50%] -translate-y-2/4 -translate-x-2/4 text-[3vmin]'>
+        <div className='flex items-center justify-center h-[10vmin]'>
         <input value={city} onKeyPress={Weather} 
         onChange={e => setCity(e.target.value)} 
         className = 'border' type='text' 
         placeholder='location...'></input>
-        <div className='fixed top-[53%] left-[90%] -translate-y-2/4 -translate-x-2/4 '>
+
         <button onClick={Weather}><AiOutlineSearch />
         </button>
-        </div> 
+        
       </div>
-  <div className='flex justify-around fixed bottom-0 w-full'>
+  <div className='flex justify-around absolute bottom-0 w-full h-[auto]'>
      {weatherData && weatherData.main && <h1 className='text-center text-[3vmin]'> {weatherData.main.humidity} %<h1 className='font-bold text-[2.5vmin]'>Humidity</h1> </h1> }
      {weatherData && weatherData.main && <h1 className=' text-center text-[3vmin]'> {weatherData.main.feels_like} °F<h1 className='font-bold text-[2.5vmin]'>Feels Like</h1> </h1>  }
      {weatherData && weatherData.wind && <h1 className='text-center text-[3vmin]'> {weatherData.wind.speed} MPH<h1 className='font-bold text-[2.5vmin]'>Wind Speed</h1> </h1>  }
     
     
   </div>
-    </> 
+    </div> 
   );
 }
 
